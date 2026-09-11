ea9682a3001869ef9544cbfad3d7867c111fa6be
vidhika jain <jainvidhika410@gmail.com>
2026-09-11 17:00:51 +0530

    Create initial App component with layout

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+```tsx
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header>
+        <h1>Canvasly</h1>
+        <button>Clear</button>
+      </header>
+
+      <main>
+        <canvas />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+```
