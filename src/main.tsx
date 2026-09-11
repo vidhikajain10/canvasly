6bcf6e174f08050106930ce2fcab8748689ef4af
vidhika jain <jainvidhika410@gmail.com>
2026-09-11 17:02:22 +0530

    Add main.tsx for React application entry point

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+```tsx
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
+```
